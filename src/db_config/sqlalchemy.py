f3d6b63dbf330f8ae7b65dd385ceb1cdaa35f724
jaineil <mandaviajaineil@gmail.com>
2022-05-08 17:33:37 -0700

    update: established remote conn

--- src/db_config/sqlalchemy.py
+++ src/db_config/sqlalchemy.py
@@ -1,5 +1,6 @@
 from .credentials import user, password, host, port, database
 from sqlalchemy import create_engine
+import pymysql
 
 
 def get_sqlalchemy_connection():
@@ -7,7 +8,9 @@ def get_sqlalchemy_connection():
         engine = create_engine(
             url=f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
         )
-        print(f"Connection to the {host} for user {user} created successfully!")
+        print(
+            f"Connection to the {host} for user {user} created successfully using ORM."
+        )
         return engine
     except Exception as ex:
         print(f"Connection could not be made due to the following error: {ex}")
--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
-from .db_config import get_mysql_connection
-from .db_config import get_sqlalchemy_connection
+from db_config import get_mysql_connection, get_sqlalchemy_connection
 
 get_mysql_connection()
 get_sqlalchemy_connection()
