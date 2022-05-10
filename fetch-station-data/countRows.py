e60e7a927a61f5b1f38aa67b1e9802b4672d9457
jash-05 <jash.sheth@gmail.com>
2022-05-09 21:57:31 -0700

    count rows

---
+++ fetch-station-data/countRows.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+totalRows = 0
+
+for i in range(1, 11):
+    filename = f"data/{i}.csv"
+    data = pd.read_csv(filename)
+    rows = len(data)
+    print(rows)
+    totalRows += rows
+
+print(f"\nTotal Rows: {totalRows}")
